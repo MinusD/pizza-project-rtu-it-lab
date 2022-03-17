48d7b68eb35fab70a5334f753eb857bc2078d411
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-03-17 18:13:52 +0300

    Add Cross-env Add DB connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = Number(process.env.PROJECT_PORT) || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+      .setTitle('MBC Studio')
+      .setDescription('Сервис для автоматизации работы пиццерии')
+      .setVersion('1.0.0')
+      .addTag('Документация')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 bootstrap();
